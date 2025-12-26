36aed3a571ce89844e011bb3d896ba102b40fbe1
Dhruv <dhruvrai28069@gmail.com>
2025-12-26 23:32:17 +0530

    Problem 1304

---
+++ Prob_1304.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class Prob_1304 {
+
+    public int[] sumZero(int n) {
+
+        int[] result = new int[n];
+        int i = 0;
+        int j = n-1;
+
+        while(i < j){
+            result[i] = -(i + 1);
+            result[j] = (i + 1);
+            i++;
+            j--;
+        }
+        return result;
+    }
+
+    static void main() {
+
+         Prob_1304 Example = new Prob_1304();
+        int n = 5;
+        int[] output = Example.sumZero(n);
+
+        System.out.println("Input: " + n);
+        System.out.println("Output: " + Arrays.toString(output));
+    }
+}
