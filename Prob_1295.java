de3519b7f9291e99d99ebacf4030930f30062af7
Dhruv <dhruvrai28069@gmail.com>
2025-12-26 23:32:56 +0530

    Problem 1304

--- Prob_1295.java
+++ Prob_1295.java
@@ -1,21 +1,2 @@
 public class Prob_1295 {
     public int findNumbers(int[] nums) {
-
-        int count = 0;
-        int n = nums.length;
-        int answer = 0;
-
-        for (int i = 0; i < n; i++) {
-            int x = nums[i];
-            count = 0;
-            while (x != 0) {
-                count++;
-                x = x / 10;
-            }
-            if (count % 2 == 0) {
-                answer++;
-            }
-        }
-        return answer;
-    }
-}
