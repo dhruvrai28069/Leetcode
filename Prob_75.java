ed0986b92919ee9d36abc368ac8c08217e3171e0
Dhruv <dhruvrai28069@gmail.com>
2025-12-28 00:04:42 +0530

    Problem 75

---
+++ Prob_75.java
@@ -0,0 +1,32 @@
+public class Prob_75 {
+
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public void sortColors(int[] nums) {
+
+        int n = nums.length;
+        int low = 0;
+        int curr = 0;
+        int high = n - 1;
+        while(curr <= high){
+            if(nums[curr] == 0){
+                swap(nums,curr,low);
+                curr++;
+                low++;
+            }
+
+            else if(nums[curr] == 1){
+                curr++;
+            }
+
+            else if(nums[curr] == 2){
+                swap(nums,curr,high);
+                high--;
+            }
+        }
+    }
+}
