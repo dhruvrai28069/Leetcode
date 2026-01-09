27a322360ee0a821234fc20597a7bf146baffeff
Dhruv <dhruvrai28069@gmail.com>
2026-01-10 00:10:27 +0530

    Problem 217

---
+++ Prob_217.java
@@ -0,0 +1,25 @@
+// USING HASHMAP:
+
+import java.util.HashMap;
+
+public class Prob_217{
+    public boolean containsDuplicate(int[] nums) {
+
+        HashMap<Integer , Integer>map = new HashMap<>();
+        int n = nums.length;
+
+        for(int i = 0 ; i<n ; i++){
+            if(map.containsKey(nums[i])){
+                return true;
+            }
+            map.put(nums[i],i);
+        }
+        return false;
+    }
+     static void main(String[] args) {
+        Prob_217 solution = new Prob_217();
+
+        int[] testArray = {1, 2, 3, 1};
+        System.out.println("Contains duplicate: " + solution.containsDuplicate(testArray));
+    }
+}
