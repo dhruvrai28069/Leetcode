035b626c2c3284dd17c1b05c2965e918c63dc849
Dhruv <dhruvrai28069@gmail.com>
2026-02-11 22:07:16 +0530

    Problem 344

---
+++ Prob_344.java
@@ -0,0 +1,20 @@
+public class Prob_344 {
+
+    public void reverseString(char[] s) {
+        int n = s.length;
+        int i = 0;
+        int j = n - 1;
+
+        while (i < j) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+
+            i++;
+            j--;
+        }
+    }
+    static void main(){
+
+    }
+}
