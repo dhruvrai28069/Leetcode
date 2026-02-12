5f183b54c1da5b852953c4a2d778658f95ca9faf
Dhruv <dhruvrai28069@gmail.com>
2026-02-12 22:50:31 +0530

    Problem 345

---
+++ Prob_345.java
@@ -0,0 +1,35 @@
+public class Prob_345 {
+
+    public boolean isVowel(char ch) {
+        ch = Character.toLowerCase(ch);
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+
+    public String reverseVowels(String s) {
+        char[] arr = s.toCharArray();
+        int i = 0;
+        int j = arr.length - 1;
+
+        while (i < j) {
+
+            while (i < j && !isVowel(arr[i])) {
+                i++;
+            }
+
+            while (i < j && !isVowel(arr[j])) {
+                j--;
+            }
+
+            char temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+
+            i++;
+            j--;
+        }
+        return new String(arr);
+    }
+    static void main(){
+
+    }
+}
