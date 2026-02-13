5afa83060506ec8b65470a5c999e2700e4591c41
Dhruv <dhruvrai28069@gmail.com>
2026-02-13 19:54:29 +0530

    Problem 125

---
+++ Prob_125.java
@@ -0,0 +1,33 @@
+public class Prob_125 {
+
+    public boolean isPalindrome(String s) {
+
+        s = s.toLowerCase();
+        int n = s.length();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+
+            if ((ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')) {
+                sb.append(ch);
+            }
+        }
+
+        int filteredN = sb.length();
+        int i = 0;
+        int j = filteredN - 1;
+
+        while (i < j) {
+            if (sb.charAt(i) != sb.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    static void main(){
+
+    }
+}
