4ea90f1010adfe80f1ee984ce23db77ed2b7718d
Dhruv <dhruvrai28069@gmail.com>
2026-03-04 16:04:41 +0530

    Problem 153

---
+++ Prob_153.java
@@ -0,0 +1,22 @@
+public class Prob_153 {
+
+    public int findMin(int[] nums) {
+
+        int n = nums.length;
+        int low = 0;
+        int high = n - 1;
+
+        while(low < high){
+
+            int mid = low + (high - low)/2;
+
+            if(nums[mid] > nums[high]){
+                low = mid + 1;
+            }
+            else{
+                high = mid;
+            }
+        }
+        return nums[high];
+    }
+}
