70c0fa56e1f428d17cd5d296072a9377e35f83fe
Dhruv <dhruvrai28069@gmail.com>
2026-03-06 22:44:23 +0530

    Problem 367

---
+++ Prob_367.java
@@ -0,0 +1,22 @@
+public class Prob_367 {
+
+    public boolean isPerfectSquare(int num) {
+
+        long low = 1;
+        long high = num;
+
+        while(low <= high){
+            long mid = low + (high - low)/2;
+            if(mid*mid == num){
+                return true;
+            }
+            else if(mid*mid > num){
+                high = mid - 1;
+            }
+            else{
+                low= mid + 1;
+            }
+        }
+        return false;
+    }
+}
