e17f8c557034f625c41517433cf3c4e1cd1ae89b
Dhruv <dhruvrai28069@gmail.com>
2026-03-17 23:42:14 +0530

    Problem 2

--- Prob_2.java
+++ Prob_2.java
@@ -3,9 +3,7 @@ public class Prob_2 {
     public static class ListNode {
         int val;
         ListNode next;
-        ListNode() {}
         ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
