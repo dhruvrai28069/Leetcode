505d39e941024193c850364e63a9149b7ff1a237
Dhruv <dhruvrai28069@gmail.com>
2026-03-25 14:58:14 +0530

    Problem 19

---
+++ Prob_19.java
@@ -0,0 +1,29 @@
+public class Prob_19 {
+
+    public static class ListNode {
+        int val;
+        Prob_19.ListNode next;
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return dummy.next;
+    }
+}
