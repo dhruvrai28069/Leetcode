b7cf34e737c4c0f6419bce6362cfbc23b610526c
Dhruv <dhruvrai28069@gmail.com>
2026-04-04 21:57:00 +0530

    Problem 402

---
+++ Prob_402.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Prob_402 {
+
+    public String removeKdigits(String num, int k) {
+
+        int n = num.length();
+        Stack<Character> st = new Stack<>();
+        for(int i =0; i<n; i++){
+            char ch = num.charAt(i);
+            while(!st.isEmpty() && k > 0 && st.peek() > ch){
+                st.pop();
+                k--;
+            }
+            st.push(ch);
+        }
+        while(k > 0){
+            st.pop();
+            k--;
+        }
+        StringBuilder sb = new StringBuilder();
+
+        while(!st.isEmpty()){
+            sb.append(st.pop());
+        }
+        sb.reverse();
+
+        while(sb.length() > 0 && sb.charAt(0) == '0'){
+            sb.deleteCharAt(0);
+        }
+        if(sb.length() == 0){
+            return "0";
+        }
+        else{
+            return sb.toString();
+        }
+    }
+}
