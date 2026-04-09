4b904b0ded8983e1052a057475a8876defcb7a41
Dhruv <dhruvrai28069@gmail.com>
2026-04-09 23:40:37 +0530

    Problem 622

---
+++ Prob_622.java
@@ -0,0 +1,68 @@
+public class Prob_622 {
+}
+class MyCircularQueue {
+
+    int size;
+    int currsize;
+    int front;
+    int rear;
+    int[] arr;
+
+    public MyCircularQueue(int k) {
+        this.size = k;
+        arr = new int[k];
+        front = -1;
+        rear = -1;
+        currsize = 0;
+    }
+
+    public boolean enQueue(int value) {
+        if(isFull()) return false;
+
+        if (isEmpty()) {
+            front = 0;
+        }
+
+        rear = (rear + 1) % size;
+        arr[rear] = value;
+        currsize++;
+        return true;
+    }
+
+    public boolean deQueue() {
+        if(isEmpty()) return false;
+
+        front = (front + 1) % size;
+        currsize--;
+
+        if(currsize == 0){
+            front = -1;
+            rear = -1;
+        }
+        return true;
+    }
+
+    public int Front() {
+
+        if(isEmpty()){
+            return -1;
+        }
+        return arr[front];
+    }
+
+    public int Rear() {
+
+        if(isEmpty()){
+            return -1;
+        }
+        return arr[rear];
+    }
+
+    public boolean isEmpty() {
+        return currsize == 0;
+    }
+
+    public boolean isFull() {
+        return currsize == size;
+    }
+}
