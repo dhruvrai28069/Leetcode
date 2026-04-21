0f88202eeece422121a145bc447c102feca66bf0
Dhruv <dhruvrai28069@gmail.com>
2026-04-21 23:55:10 +0530

    Problem 46

---
+++ Prob_46.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Prob_46 {
+
+    public void solve(int[] arr, int idx, List<List<Integer>> ans){
+        if(idx == arr.length){
+            List<Integer> list = new ArrayList<>();
+            for(int a : arr){
+                list.add(a);
+            }
+            ans.add(list);
+            return;
+        }
+
+        for(int i = idx; i < arr.length; i++){
+            swap(arr, i, idx);
+            solve(arr, idx + 1, ans);
+            swap(arr, i, idx);
+        }
+    }
+
+    public void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public List<List<Integer>> permute(int[] arr) {
+        List<List<Integer>> ans = new ArrayList<>();
+        solve(arr, 0, ans);
+        return ans;
+    }
+}
