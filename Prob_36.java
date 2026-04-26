cf9f7d1bc2a93ff9d306c762ee46075c410830e9
Dhruv <dhruvrai28069@gmail.com>
2026-04-26 23:14:12 +0530

    Problem 36

---
+++ Prob_36.java
@@ -0,0 +1,28 @@
+public class Prob_36 {
+
+    public boolean isValid(char[][] board,int row , int col , char c){
+        for(int i = 0; i < 9; i++){
+            if(board[i][col] == c)return false;
+            if(board[row][i] == c) return false;
+
+            if(board[3 * (row/3) + i / 3][3 * (col/3) + i % 3] == c) return false;
+        }
+        return true;
+
+    }
+    public boolean isValidSudoku(char[][] board) {
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                if(board[i][j] != '.'){
+                    char c = board[i][j];
+                    board[i][j] = '.';
+                    if(!isValid(board,i,j,c)){
+                        return false;
+                    }
+                    board[i][j] = c;
+                }
+            }
+        }
+        return true;
+    }
+}
