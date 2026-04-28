611733e4f5b2fef8c814b22080e0aefa03affdd5
Dhruv <dhruvrai28069@gmail.com>
2026-04-28 23:04:51 +0530

    Problem 37

---
+++ Prob_37.java
@@ -0,0 +1,35 @@
+public class Prob_37 {
+
+    public boolean isValid(char[][] board, int row, int col, char k){
+        for(int i = 0; i < 9; i++){
+            if(board[i][col] == k) return false;
+            if(board[row][i] == k) return false;
+            if(board[3 * (row/ 3) + i / 3][3 * (col/3) + i % 3] == k) return false;
+        }
+        return true;
+    }
+    public boolean solve(char[][] board){
+        for(int i = 0; i < board.length;i++){
+            for(int j = 0; j < board[0].length;j++){
+                if(board[i][j] == '.'){
+                    for(char k = '1'; k <= '9'; k++){
+                        if(isValid(board,i,j,k)){
+                            board[i][j] = k;
+                            if(solve(board)){
+                                return true;
+                            }
+                            else{
+                                board[i][j] = '.';
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+}
