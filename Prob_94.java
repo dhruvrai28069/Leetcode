fcc417793b90f9eed96f0616253883e793ef74a8
Dhruv <dhruvrai28069@gmail.com>
2026-05-16 22:47:33 +0530

    Problem 94

--- Prob_94.java
+++ Prob_94.java
@@ -8,8 +8,10 @@ public class Prob_94 {
         TreeNode left;
         TreeNode right;
         TreeNode() {}
-        TreeNode(int val) { this.val = val; }
-        TreeNode(int val, TreeNode left, TreeNode right) {
+        TreeNode(int val){
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right){
             this.val = val;
             this.left = left;
             this.right = right;
