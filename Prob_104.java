23a659c0adc612aaf979ae8d561f7d43aac6117c
Dhruv <dhruvrai28069@gmail.com>
2026-05-23 22:44:35 +0530

    Problem 104

---
+++ Prob_104.java
@@ -0,0 +1,18 @@
+public class Prob_104 {
+
+    public class TreeNode {
+        int val;
+        Prob_104.TreeNode left;
+        Prob_104.TreeNode right;
+
+    }
+    public int maxDepth(TreeNode root) {
+
+        if(root == null) return 0;
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return 1 + Math.max(left, right);
+    }
+}
