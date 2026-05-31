6f98e648bf0a6ba2c1ff9463ee92dbe07175f8d5
Dhruv <dhruvrai28069@gmail.com>
2026-06-01 00:31:37 +0530

    Problem 701

---
+++ Prob_701.java
@@ -0,0 +1,39 @@
+public class Prob_701 {
+
+    public class TreeNode {
+        int val;
+        Prob_701.TreeNode left;
+        Prob_701.TreeNode right;
+        TreeNode(int val){
+            this.val = val;
+        }
+    }
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+
+        if(root == null) return new TreeNode(val);
+
+        TreeNode temp = root;
+        while(true){
+            if(temp.val < val){
+                if(temp.right != null){
+                    temp = temp.right;
+                }
+                else{
+                    temp.right = new TreeNode(val);
+                    break;
+                }
+            }
+            else{
+                if(temp.left != null){
+                    temp = temp.left;
+                }
+                else{
+                    temp.left = new TreeNode(val);
+                    break;
+                }
+            }
+        }
+        return root;
+    }
+}
