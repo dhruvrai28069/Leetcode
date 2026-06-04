373b4dd5d58a56c527ce125648e85def42bfc36f
Dhruv <dhruvrai28069@gmail.com>
2026-06-04 23:49:23 +0530

    Problem 236

---
+++ Prob_236.java
@@ -0,0 +1,24 @@
+public class Prob_236 {
+
+    public class TreeNode{
+        int val;
+        Prob_236.TreeNode left;
+        Prob_236.TreeNode right;
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode leftNode = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightNode = lowestCommonAncestor(root.right, p, q);
+
+        if (leftNode != null && rightNode != null) {
+            return root;
+        }
+
+        return leftNode != null ? leftNode : rightNode;
+    }
+}
