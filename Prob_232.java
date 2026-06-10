a5a75e33ae1b61eac6163bba39ca532d62b001ec
Dhruv <dhruvrai28069@gmail.com>
2026-06-11 00:00:00 +0530

    Problem 232

---
+++ Prob_232.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+public class Prob_232 {
+
+    private Stack<Integer> inputStack;
+    private Stack<Integer> outputStack;
+
+    public Prob_232() {
+        inputStack = new Stack<>();
+        outputStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        inputStack.push(x);
+    }
+
+    public int pop() {
+        peek();
+        return outputStack.pop();
+    }
+
+    public int peek() {
+        if (outputStack.isEmpty()) {
+            while (!inputStack.isEmpty()) {
+                outputStack.push(inputStack.pop());
+            }
+        }
+        return outputStack.peek();
+    }
+
+    public boolean empty() {
+        return inputStack.isEmpty() && outputStack.isEmpty();
+    }
+}
