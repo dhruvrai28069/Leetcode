3b294c3de5660a666ad86891d742da09f43d3769
Dhruv <dhruvrai28069@gmail.com>
2026-07-17 22:25:43 +0530

    Problem 86

---
+++ Prob_86.java
@@ -0,0 +1,42 @@
+public class Prob_86 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {}
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode partition(ListNode head, int x) {
+        ListNode beforeHead = new ListNode(0);
+        ListNode before = beforeHead;
+        ListNode afterHead = new ListNode(0);
+        ListNode after = afterHead;
+
+        while (head != null) {
+            if (head.val < x) {
+                before.next = head;
+                before = before.next;
+            } else {
+                after.next = head;
+                after = after.next;
+            }
+            head = head.next;
+        }
+
+        after.next = null;
+
+        before.next = afterHead.next;
+
+        return beforeHead.next;
+    }
+}
