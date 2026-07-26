29cc14ad55445f4193ec996dfaffbf998c64f793
Dhruv <dhruvrai28069@gmail.com>
2026-07-27 00:08:51 +0530

    Problem 121

---
+++ Prob_121.java
@@ -0,0 +1,21 @@
+public class Prob_121 {
+
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else {
+                maxProfit = Math.max(maxProfit, price - minPrice);
+            }
+        }
+
+        return maxProfit;
+    }
+}
