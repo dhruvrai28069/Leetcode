7a6caf617fc6f6d8453df15e5f21db3c8dd5f66f
Dhruv <dhruvrai28069@gmail.com>
2026-08-19 22:39:16 +0530

    Problem 209

---
+++ Prob_209.java
@@ -0,0 +1,22 @@
+public class Prob_209 {
+
+    public int minSubArrayLen(int target, int[] nums) {
+        int minLen = Integer.MAX_VALUE;
+        int left = 0;
+        int currentSum = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+
+            currentSum += nums[right];
+
+            while (currentSum >= target) {
+
+                minLen = Math.min(minLen, right - left + 1);
+                currentSum -= nums[left];
+                left++;
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE ? 0 : minLen;
+    }
+}
